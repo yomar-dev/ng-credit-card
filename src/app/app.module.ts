3c61b9b12a51b9f71103c00024cb1ce3acf941a4
Yomar Miranda <mirandayomar@gmail.com>
2021-08-16 16:58:00 -0500

    feat: generate card-form component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardFormComponent } from './card-form/card-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardFormComponent
   ],
   imports: [
     BrowserModule
