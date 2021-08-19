699ace5901f320c3ae258e735a106237980319f9
Yomar Miranda <mirandayomar@gmail.com>
2021-08-18 21:56:23 -0500

    feat: remove NgxMask config from AppModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,13 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { NgxMaskModule, IConfig } from 'ngx-mask';
-
 import { AppComponent } from './app.component';
 import { CardFormComponent } from './card-form/card-form.component';
 import { InputComponent } from './input/input.component';
 
-export const options: Partial<IConfig> | (() => Partial<IConfig>) = {};
-
 @NgModule({
   declarations: [AppComponent, CardFormComponent, InputComponent],
-  imports: [BrowserModule, ReactiveFormsModule, NgxMaskModule.forRoot(options)],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
