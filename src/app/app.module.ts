f43a184863b329c0c4cd357ee5965411c15ee09b
Yomar Miranda <mirandayomar@gmail.com>
2021-08-18 21:41:49 -0500

    chore: install and config ngx-mask.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NgxMaskModule, IConfig } from 'ngx-mask';
+
 import { AppComponent } from './app.component';
 import { CardFormComponent } from './card-form/card-form.component';
 import { InputComponent } from './input/input.component';
 
+export const options: Partial<IConfig> | (() => Partial<IConfig>) = {};
+
 @NgModule({
   declarations: [AppComponent, CardFormComponent, InputComponent],
-  imports: [BrowserModule, ReactiveFormsModule],
+  imports: [BrowserModule, ReactiveFormsModule, NgxMaskModule.forRoot(options)],
   providers: [],
   bootstrap: [AppComponent],
 })
